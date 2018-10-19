d04b4bcc87d792573e47bf4947b49f5d69688265
Collin Bradley <herescollin@gmail.com>
2018-10-19 20:23:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // add solution here
+function theBeatlesPlay(musicians,instruments){
+  
+}
+
+var myVariable = []
+
+for(i=0;i<musicians.length;i++){
+  `${musicians[i]} plays ${instruments[i]}`
+}
