68a1a1f1c45350b2e9620bbb7838dfb38619b160
Collin Bradley <herescollin@gmail.com>
2018-10-19 20:24:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 // add solution here
 function theBeatlesPlay(musicians,instruments){
-  
+  var myVariable = []
+
+  for(i=0;i<musicians.length;i++){
+    myVariable.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
 }
 
-var myVariable = []
-
-for(i=0;i<musicians.length;i++){
-  `${musicians[i]} plays ${instruments[i]}`
-}
